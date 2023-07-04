4e3f451e7624ccbcc36f6d61670bb869e9b5a380
Godwin-Akpa <godwindevops@gmail.com>
2023-07-04 21:12:32 +0100

    C is foundation

---
+++ for_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	for (x = 0; x < 20; x++)
+	{
+	printf("%d", x);
+	}
+	return 0;
+}
---
+++ y.c
@@ -0,0 +1,7 @@
+int main()
+a = 0;
+do while (a < b)
+{
+    printf("%d\n", a);
+    a++;
+}
